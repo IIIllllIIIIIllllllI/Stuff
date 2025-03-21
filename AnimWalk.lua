2ddb78798304fa92b97b8916b4f51d0b71e3fc2c
noober <gewsrqwerawfdhdsrrwerdasf@gmail.com>
2025-03-21 19:35:35 +0100

    yay

---
+++ AnimWalk.lua
@@ -0,0 +1,13 @@
+local chr = game.Players.LocalPlayer.Character
+local hum = chr and chr:FindFirstChildWhichIsA("Humanoid")
+local hb = game:GetService("RunService").Heartbeat
+
+chr.Animate.Enabled = false
+for i = 1, 3 do
+while chr and hum and hum.Parent do
+    local delta = hb:Wait()
+    if hum.MoveDirection.Magnitude > 0 then
+        chr:TranslateBy(hum.MoveDirection * delta * 5)
+    end
+end
+end
